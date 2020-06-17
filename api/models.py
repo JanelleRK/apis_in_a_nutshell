d94951696c12f86b764d4de64c5a8446cd800dd8
Janelle Kuhns <j3ckuhns@gmail.com>
2020-06-17 13:06:55 -0700

    added bootstrap_data.py

--- api/models.py
+++ api/models.py
@@ -5,13 +5,63 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=30)
     website = models.URLField(max_length= 300, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class ShoeType(models.Model):
-    style = models.CharField(max_length=40)
+    SNEAKER = 'Sneaker'
+    BOOT = 'Boot'
+    SANDAL = 'Sandal'
+    DRESS = 'Dress'
+    OTHER = 'Other'
+    SHOE_TYPE_CHOICES = [
+        (SNEAKER, 'Sneaker'),
+        (BOOT, 'Boot'),
+        (SANDAL, 'Sandal'),
+        (DRESS, 'Dress'),
+        (OTHER, 'Other'),
+    ]
+    style = models.CharField(
+        max_length=7,
+        choices=SHOE_TYPE_CHOICES,
+        default=SNEAKER,
+    )
+
+    def __str__(self):
+        return self.style
+
 
 
 class ShoeColor(models.Model):
-    color_name = models.CharField(max_length=30)
+    RED = 'Red'
+    ORANGE = 'Orange'
+    YELLOW = 'Yellow'
+    GREEN = 'Green'
+    BLUE = 'Blue'
+    INDIGO = 'Indigo'
+    VIOLET = 'Violet'
+    WHITE = 'White'
+    BLACK = 'Black'
+    SHOE_COLOR_CHOICES = [
+        (RED, 'Red'),
+        (ORANGE, 'Orange'),
+        (YELLOW, 'Yellow'),
+        (GREEN, 'Green'),
+        (BLUE, 'Blue'),
+        (INDIGO, 'Indigo'),
+        (VIOLET, 'Violet'),
+        (WHITE, 'White'),
+        (BLACK, 'Black'),
+    ]
+    color_name = models.CharField(
+        max_length=6,
+        choices=SHOE_COLOR_CHOICES,
+        default=BLACK,
+    )
+
+    def __str__(self):
+        return self.color_name
 
 
 class Shoe(models.Model):
@@ -22,3 +72,8 @@ class Shoe(models.Model):
     material = models.CharField(max_length=30)
     shoe_type = models.ForeignKey(ShoeType, on_delete=models.CASCADE)
     fasten_type = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.shoe_type
+
+#Probably not true but I heard that Joe was raised by hyenas and that is why he has such an awesome and contagious laugh.
---
+++ management/commands/bootstrap_data.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.models import ShoeType, ShoeColor
+
+class Command(BaseCommand):
+    help = 'Creates a shoe type and shoe color'
+
+    def handle(self):
+
+        ShoeType.objects.create(style = 'Sneaker')
+        ShoeType.objects.create(style='Boot')
+        ShoeType.objects.create(style='Sandal')
+        ShoeType.objects.create(style='Dress')
+        ShoeType.objects.create(style='Other')
+
+        ShoeColor.objects.create(color_name='Red')
+        ShoeColor.objects.create(color_name='Orange')
+        ShoeColor.objects.create(color_name='Yellow')
+        ShoeColor.objects.create(color_name='Green')
+        ShoeColor.objects.create(color_name='Blue')
+        ShoeColor.objects.create(color_name='Indigo')
+        ShoeColor.objects.create(color_name='Violet')
+        ShoeColor.objects.create(color_name='White')
+        ShoeColor.objects.create(color_name='Black')
+
+
+
+
